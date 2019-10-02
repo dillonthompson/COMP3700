93f9e523a736218744d39f37a96dc8aed66a96ea
Dillon Thompson <dj.thompson715@gmail.com>
2019-10-02 17:01:56 -0500

    finshed activity 11

--- activity11/CustomerModel.java
+++ activity11/CustomerModel.java
@@ -1,14 +1,15 @@
+package edu.auburn;
+
 public class CustomerModel {
-    public int mProductID;
-    public String mName, mVendor, mDescription;
-    public double mPrice, mQuantity;
+    public int mcustomerID;
+    public String mName, mAddress, mPhone;
 
     public String toString() {
         StringBuilder sb = new StringBuilder("(");
-        sb.append(mProductID).append(",");
+        sb.append(mcustomerID).append(",");
         sb.append("\"").append(mName).append("\"").append(",");
-        sb.append(mPrice).append(",");
-        sb.append(mQuantity).append(")");
+        sb.append("\"").append(mAddress).append("\"").append(",");
+        sb.append("\"").append(mPhone).append("\"").append(")");
         return sb.toString();
     }
 }
